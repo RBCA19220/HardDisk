1b41bb9a0fb6ee64cda27ce965788ac6387afa86
sysadmin <sysadmin@bcalab1-m06>
2021-04-08 11:44:43 +0530

    master push

---
+++ src/Folder.java
@@ -0,0 +1,17 @@
+
+public class Folder {
+	String name;
+	int numfiles;
+	
+	Folder(String name, int numfiles)
+	{
+		this.name=name;
+		this.numfiles=numfiles;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
